6a0883aa8517713121c5ba164ce3abf727425ba9
taniac101 <taniac@email.arizona.edu>
2020-01-28 10:04:22 -0700

    changed_hello01

--- exercises/01_how_to_write/hello01.py
+++ exercises/01_how_to_write/hello01.py
@@ -1,1 +1,15 @@
-print('Hello, World!')
+#!/usr/bin/env python3
+# Purpose: Say hello
+
+import sys
+
+def greet(name):
+    return f'Hello, {name}!'
+
+def test_greet():
+    assert greet('World') == 'Hello, World!'
+    assert greet('Terra Firma') == 'Hello, Terra Firma!'
+
+args = sys.argv[1:]
+name = args[0] if args else 'World'
+print(greet(name))
